1ddafd616bb1d65e654d0ace0413a4b6add5969f
MIUNO <miunodevel@gmail.com>
2021-12-15 20:54:08 +0300

    fix

--- src/main/java/ru/miuno/nicemod/blocks/Capital.java
+++ src/main/java/ru/miuno/nicemod/blocks/Capital.java
@@ -4,20 +4,41 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Material;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.block.Waterloggable;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 
-public class Capital extends Block {
-
-    @Override
-    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return VoxelShapes.union(VoxelShapes.cuboid(0, 0, 0, 1, 0.125, 1), VoxelShapes.cuboid(0.125, 0.125, 0.125, 0.750, 1, 0.875), VoxelShapes.cuboid(0, 0.875, 0, 1, 1, 1));
-    }
+public class Capital extends Block implements Waterloggable {
+    public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
 
     protected Capital(Settings settings) {
         super(Settings.of(Material.STONE).nonOpaque());
+        this.setDefaultState((BlockState)((BlockState)((BlockState)this.stateManager.getDefaultState())).with(WATERLOGGED, false));
     }
-    
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return VoxelShapes.union(VoxelShapes.cuboid(0, 0, 0, 1, 0.125, 1), VoxelShapes.cuboid(0.125, 0.125, 0.125, 0.875, 1, 0.875), VoxelShapes.cuboid(0, 0.875, 0, 1, 1, 1));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(WATERLOGGED);
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        if (state.get(WATERLOGGED).booleanValue()) {
+            return Fluids.WATER.getStill(false);
+        }
+        return Fluids.EMPTY.getDefaultState();
+    }
+
 }
