896dc2d906e14600a3a3014c12e50b136154c415
MIUNO <miunodevel@gmail.com>
2021-12-15 20:04:06 +0300

    a

--- src/main/java/ru/miuno/nicemod/blocks/Capital.java
+++ src/main/java/ru/miuno/nicemod/blocks/Capital.java
@@ -1,9 +1,20 @@
 package ru.miuno.nicemod.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Material;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 
 public class Capital extends Block {
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return VoxelShapes.union(VoxelShapes.cuboid(0, 0, 0, 1, 0.125, 1), VoxelShapes.cuboid(0.125, 0.125, 0.125, 0.750, 1, 0.875), VoxelShapes.cuboid(0, 0.875, 0, 1, 1, 1));
+    }
 
     protected Capital(Settings settings) {
         super(Settings.of(Material.STONE).nonOpaque());
