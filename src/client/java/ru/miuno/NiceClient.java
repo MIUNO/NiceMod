5c170c379bed022d79b5b801e01eaad04d02e905
MIUNO <miunodevel@gmail.com>
2024-07-13 23:26:26 +0300

    update 1.4.2

--- src/client/java/ru/miuno/NiceClient.java
+++ src/client/java/ru/miuno/NiceClient.java
@@ -10,7 +10,7 @@ public class NiceClient implements ClientModInitializer  {
 	@Override
 	public void onInitializeClient() {
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(),
-		NiceBlock.OAK_WALLLANTERN, NiceBlock.OAK_WALLLANTERNA, NiceBlock.OAK_WINDOW, NiceBlock.OAK_DECORE1, 
+		NiceBlock.OAK_WALLLANTERN, NiceBlock.OAK_WALLLANTERNA, NiceBlock.OAK_WINDOW, NiceBlock.OAK_DECORE1,
 		NiceBlock.SPRUCE_WALLLANTERN, NiceBlock.SPRUCE_WALLLANTERNA, NiceBlock.SPRUCE_WINDOW, NiceBlock.SPRUCE_DECORE1,
 		NiceBlock.WEATHERCOCK, NiceBlock.GLOBE,
 		NiceBlock.FLOWERBED_ROSE_BUSH, NiceBlock.FLOWERBED_PEONY, NiceBlock.FLOWERBED_LILAC, NiceBlock.FLOWERBED_BLUE_ORCHID, NiceBlock.FLOWERBED_RED_TULIP, NiceBlock.FLOWERBED_DANDELION, NiceBlock.FLOWERBED_OXEYE_DAISY,
